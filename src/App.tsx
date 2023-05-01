e393c769a0c944ee754909c27363a4dffc08b7cb
jt3698 <jasontedjosoesilo@gmail.com>
2023-05-01 12:01:30 +0700

    Added AWS cognito sign in template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'
 
 function App() {
+  const env = process.env.NODE_ENV;
+
   return (
     <div className="App">
       <h1>DocLock</h1>
+      <p>Environment: {env}</p>
+      <Authenticator>
+        {({ signOut }) => (
+            <div>
+              <h1>Welcome to my Website!</h1>
+              <h3>You are authenticated</h3>
+              <button onClick={signOut}>Sign Out</button>
+            </div>
+        )}
+      </Authenticator>  
     </div>
   );
 }

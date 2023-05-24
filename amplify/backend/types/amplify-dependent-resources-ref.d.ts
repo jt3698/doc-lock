a461d2ff810f72417c91654bcbe98f8e6d0de3b6
jt3698 <jasontedjosoesilo@gmail.com>
2023-05-24 22:08:02 +0700

    relink appsync apis

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "DocLock": {
+    "doclock": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
